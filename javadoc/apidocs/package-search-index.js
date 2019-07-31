45f11e074317aa38b8b2f0d37ba423d1b5d0e84b
Greg Scullard <greg@scullard.com>
2019-07-31 23:06:45 +0200

    v0.5.3 uploaded to maven

--- javadoc/apidocs/package-search-index.js
+++ javadoc/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"com.hederahashgraph.api.proto.java"},{"l":"com.hederahashgraph.service.proto.java"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.hederahashgraph.api.proto.java"},{"l":"com.hederahashgraph.service.proto.java"}]
